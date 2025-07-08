f935cb5fdf34440aedc04dbd3fc2b05cd1b4e6a2
Saroj Magar <magarsaroj2430@gmail.com>
2025-07-08 13:38:54 +0545

    adding task13

---
+++ Control_Statements/task13.py
@@ -0,0 +1,8 @@
+#use loop to find factorial of a number.
+num=int(input("Enter a number: "))
+
+factorial=1
+for i in range(1,num+1):
+    factorial=factorial*i
+print(f"The factorial of {num} is {factorial}")
+
